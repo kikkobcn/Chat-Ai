21470623c842276dd9fa4e00df35ddd4e76dedf5
kiko <kikkocrypto21@gmail.com>
2023-01-22 20:38:41 +0100

    udpate server render

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://chat-ai-sld2.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
